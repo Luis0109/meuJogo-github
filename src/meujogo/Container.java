e30cb8debb649e1aeaf9ee6bc5cd0820e17d5789
Luis Henrique <luishenrique0109@hotmail.com>
2021-08-02 18:35:48 -0300

    Jogo criado

---
+++ src/meujogo/Container.java
@@ -0,0 +1,21 @@
+package meujogo;
+
+import javax.swing.JFrame;
+
+import meujogo.modelo.Fase;
+
+public class Container extends JFrame {
+	public Container() {
+		add (new Fase());
+		setTitle("Meu jogo");
+		setSize(1024,728);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(true);
+		setVisible(true);
+	}
+
+	public static void main(String [] args) {
+		new Container();
+	}
+}
